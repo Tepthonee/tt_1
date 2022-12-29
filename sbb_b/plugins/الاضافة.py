d54301ff42da5994eba855416fe1fb8ce84290bf
Tepthonee <115221367+Tepthonee@users.noreply.github.com>
2022-12-29 12:03:44 +0300

    Update الاضافة.py

--- sbb_b/plugins/الاضافة.py
+++ sbb_b/plugins/الاضافة.py
@@ -51,7 +51,7 @@ async def _(event):
 async def get_users(event):
     legen_ = event.text[10:]
     sbb_b_chat = legen_.lower
-    restricted = ["@super_jmthon", "@jmthon_support"]
+    restricted = ["@super_tepthon", "@Tepthon_Help"]
     SBB_B = await edit_or_reply(event, f"**جارِ اضأفه الاعضاء من  ** {legen_}")
     if sbb_b_chat in restricted:
         return await SBB_B.edit(
